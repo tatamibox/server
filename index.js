955287c253c6a98b97257b9ff61152e97de7720f
tatamibox <tat4mi@gmail.com>
2023-07-07 15:28:50 -0700

    use corsoptions

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const corsOptions = {
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200,
 }
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
